7fb82d2e7997c649372391df28a9c3cc477d1a7e
SheMortal <mortalshe@gmail.com>
2021-02-05 22:01:44 +0200

    Updates

---
+++ precache-manifest.3fca63fd6ae23b35b5fc63859cf28ab1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87248e0b3e8355b31cf053575835906c",
+    "url": "/IT-Logger/index.html"
+  },
+  {
+    "revision": "eec929e52b98d305e712",
+    "url": "/IT-Logger/static/css/2.4da19627.chunk.css"
+  },
+  {
+    "revision": "72c72b25e1f7a747d7e4",
+    "url": "/IT-Logger/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "eec929e52b98d305e712",
+    "url": "/IT-Logger/static/js/2.668ff7c0.chunk.js"
+  },
+  {
+    "revision": "494382bc2e1fa58818bf1f9041d8a9bc",
+    "url": "/IT-Logger/static/js/2.668ff7c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72c72b25e1f7a747d7e4",
+    "url": "/IT-Logger/static/js/main.e829e01b.chunk.js"
+  },
+  {
+    "revision": "b077849cd1d2f33a1518",
+    "url": "/IT-Logger/static/js/runtime-main.21df4f96.js"
+  }
+]);
